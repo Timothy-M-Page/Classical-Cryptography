7a1112e9168f63689a256a82ee9bf2ddf257d340
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-21 15:31:00 +0000

    A basic affine cipher.

---
+++ 3. Affine Cipher.py
@@ -0,0 +1,33 @@
+
+
+A = "abcdefghijklmnopqrstuvwxyz"
+
+def inv(a):                             # Inverse element in the group Z/26Z
+    for x in range(1,26):
+        if a*x % 26 == 1:
+            return(x)
+    print(f"{a} has no inverse mod 26")  # Error message if gcd(a,26) > 1.
+    return None
+
+def affine(a,b,p):
+    c = ""
+    for i in p:
+        if i in A:
+            c = c + A[(b + (a*A.index(i))) % 26]
+        else:
+            c = c + i
+    return(c)
+
+def affine_decrypt(a,b,c):
+    p = ""
+    x = inv(a)
+    if x is None:
+        return "Decryption failed due to no inverse"
+    for i in c:
+        if i in A:
+            p = p + A[((A.index(i) - b) * x) % 26]
+        else:
+            p = p + i
+    return(p)
+
+
