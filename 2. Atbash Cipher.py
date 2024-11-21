73a6a8eedfd252bc539735efaec7f93e9b38b87d
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-21 15:53:21 +0000

    Update 2. Atbash Cipher.py

--- 2. Atbash Cipher.py
+++ 2. Atbash Cipher.py
@@ -5,9 +5,9 @@ def Atbash(p):
     c = ""
     for l in p:
         if l in A:
-            c = c + A[25 - A.index(l)]
+            c += A[25 - A.index(l)]
         else:
-            c = c + l
-    return(c)
+            c += l
+    return c
 
 # Note that the Atbash cipher is its own inverse.
