d97025f340a78e3b815bb8d567204c0deea4bf07
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-23 12:06:31 +0000

    Update 4. Substitution Cipher.py

--- 4. Substitution Cipher.py
+++ 4. Substitution Cipher.py
@@ -1,13 +1,13 @@
 
-
 A = "abcdefghijklmnopqrstuvwxyz"
 B = "poiuytrewqasdfghjklmnbvcxz"    # The substitution alphabet
 
-def sub(p, b):
+def sub_encrypt(p, b):
     c = ""
+    p = p.lower()
     for l in p:
-        if l in A:
-            c += b[(A.index(l))]
+        if l in A:                  # For each alphabetic character l in the plaintext, replace
+            c += b[(A.index(l))]    # the letter by the corresponding letter in the substitution alphabet.
         else:
             c += l
     return c
@@ -15,8 +15,8 @@ def sub(p, b):
 def sub_decrypt(c, b):
     p = ""
     for l in c:
-        if l in A:
-            p += A[(b.index(l))]
+        if l in A:                  # For each alphabetic character l in the ciphertext, replace
+            p += A[(b.index(l))]    # the letter by the corresponding letter in the alphabet.
         else:
             p += l
     return p
