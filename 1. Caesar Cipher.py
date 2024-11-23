c3b3486f5d5aa9a53b4922076a57e4d5dfcd33f7
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-23 11:30:08 +0000

    Update 1. Caesar Cipher.py

--- 1. Caesar Cipher.py
+++ 1. Caesar Cipher.py
@@ -3,9 +3,10 @@ A = "abcdefghijklmnopqrstuvwxyz"
 
 def caesar_encrypt(p,k):
     c = ""
+    p = p.lower()
     for l in p:
-        if l in A:
-            c += A[(A.index(l) + k) % 26]
+        if l in A:                            # For each alphabetic character in the plaintext
+            c += A[(A.index(l) + k) % 26]     # shift the character k places in the alphabet.
         else:
             c += l
     return c
@@ -14,7 +15,7 @@ def caesar_decrypt(c,k):
     p = ""
     for l in c:
         if l in A:
-            p += A[(A.index(l) - k) % 26]
+            p += A[(A.index(l) - k) % 26]    # Shift the alphabetic characters back by k places. 
         else:
             p += l
     return p
