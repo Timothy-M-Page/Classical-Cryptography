3a97d0a8e1c23c40774920e6ab08578357989eb9
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-23 12:28:42 +0000

    Update 6. Vigenère Cipher.py

--- 6. Vigenère Cipher.py
+++ 6. Vigenère Cipher.py
@@ -1,28 +1,29 @@
 A = "abcdefghijklmnopqrstuvwxyz"
 
-def vigenère_encrypt(p,key):
+def vigenere_encrypt(p,key):
+    p = p.lower()
     c = ""
     i = 0
-    for l in p:
+    for l in p:                     # For each alphabetic character l in the plaintext
         if l in A:
             a = A.index(l)
-            b = key[i % len(key)]
-            d = A.index(b)
-            c += A[(a+d) % len(A)]
+            b = key[i % len(key)]   # Take the character of the key
+            d = A.index(b)          # corresponding to the character's position in the plaintext
+            c += A[(a+d) % len(A)]  # shift the letter by this value through the alphabet.
             i += 1
         else:
             c += l
     return c
 
-def vigenère_decrypt(c,key):
+def vigenere_decrypt(c,key):
     p = ""
     i = 0
-    for l in c:
+    for l in c:                     # For each alphabetic character l in the ciphertext
         if l in A:
             a = A.index(l)
             b = key[i % len(key)]
             d = A.index(b)
-            p += A[(a-d) % len(A)]
+            p += A[(a-d) % len(A)]  # analogously shift the letter back through the alphabet by the same value. 
             i += 1
         else:
             p += l
