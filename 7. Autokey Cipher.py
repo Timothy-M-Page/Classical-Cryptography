af0a413dd1746c0007db1a49326535db121eb6bd
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-23 12:35:19 +0000

    Update 7. Autokey Cipher.py

--- 7. Autokey Cipher.py
+++ 7. Autokey Cipher.py
@@ -1,15 +1,15 @@
-A = "abcdefghijklmnopqrstuvwxyz"
+A = " !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
 
 def autokey_encrypt(p, key):
-    key2 = key + p
+    key2 = key + p                    # Adjoin the plaintext to the key to form key2.
     c = ""
     i = 0
     for l in p:
-        if l in A:
+        if l in A:                     # For each character l in the plaintext
             a = A.index(l)
-            b = key2[i]
-            d = A.index(b)
-            c += A[(a+d) % len(A)]
+            b = key2[i]                # Take the character of the key
+            d = A.index(b)             # corresponding to the character's position in the plaintext
+            c += A[(a+d) % len(A)]     # shift the letter by this value through the alphabet.
             i += 1
         else:
             c += l
@@ -20,15 +20,16 @@ def autokey_decrypt(c, key):
     p = ""
     i = 0
     for l in c:
-        if l in A:
+        if l in A:                     # For each character l in the plaintext
             a = A.index(l)
             b = key2[i]
             d = A.index(b)
-            p += A[(a-d)%len(A)]
-            key2 += p[i]
-            i += 1
+            p += A[(a-d)%len(A)]      # analogously shift the letter back through the alphabet by the same value.
+            key2 += p[i]              # With each decrypted letter of the plaintext, reconstruct the original key2
+            i += 1                    # by iteratively adding each decrypted letter.
         else:
             p += l
     return p
+
 
 
