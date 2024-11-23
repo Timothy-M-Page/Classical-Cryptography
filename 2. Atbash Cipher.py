b229901b2e757c32a056edf81eb6f1cd62b3e235
Timothy-M-Page <tmpage@hotmail.co.uk>
2024-11-23 11:36:44 +0000

    Update 2. Atbash Cipher.py

--- 2. Atbash Cipher.py
+++ 2. Atbash Cipher.py
@@ -2,13 +2,14 @@
 A = "abcdefghijklmnopqrstuvwxyz"
 
 def Atbash(p):
+    p = p.lower()
     c = ""
     for l in p:
-        if l in A:
-            c += A[25 - A.index(l)]
+        if l in A:                    # For each alphabetic character in the plaintext
+            c += A[25 - A.index(l)]   # replace the character by its reflection in the string A.
         else:
             c += l
     return c
 
 # Note that the Atbash cipher is its own inverse.
-# Hence there is no need for a seperate encryption and decryption function. 
+# Hence there is no need for separate encryption and decryption functions. 
